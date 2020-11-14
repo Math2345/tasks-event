df3503f3defd6cd7baaeee61155106d54428d565
Math2345 <maxim.hramtsoff@gmail.com>
2020-11-14 09:00:07 +0300

    rewrite function event at JS

--- editor/editor.js
+++ editor/editor.js
@@ -1,16 +1,22 @@
 (function() {
 
-    const input = $('.autoExpand');
+    const input = document.querySelector('.autoExpand');
 
     function disabledEvent(event) {
+        event.preventDefault();
+
         switch (event.type) {
             case 'copy' : alert('Копирование текста заблокировано'); break;
             case 'paste' : alert('Вставка текста заблокировано'); break;
             case 'contextmenu' : alert('Контекстное меню заблокировано'); break;
         }
-
-        return false;
     }
 
-    input.on('copy paste contextmenu', disabledEvent)
+    const events = ['copy', 'paste', 'contextmenu'];
+
+    events.forEach((event) => {
+        input.addEventListener(event, disabledEvent, true);
+    })
+
+
 })();
